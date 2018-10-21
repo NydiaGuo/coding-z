8d2f33479105f21e0cf917821c613d7bb9559f4d
NI GUO <guoni0909@gmail.com>
2018-10-20 18:21:05 -0700

    ignore dist file and installed style/css/sass/url/file loaders

--- src/index.js
+++ src/index.js
@@ -2,18 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './style.scss'
 
-// const MyComponent = () => <h1> Webpack with React </h1>
 
-class Text extends React.Component {
+class Image extends React.Component {
     render(){
         return (
             <div>
-                <h2>{this.props.title}</h2>
-                <p>{this.props.text}</p>
+                <h1>Webpack</h1>
+                <div id="imageDiv">
+                    <div id="image"></div>
+                  
+                </div>
+                   
+
             </div>
         )
     }
 }
 
-ReactDOM.render(<Text title="Call Alex" text="Can you call him at 3pm?" />, 
-                document.getElementById('react-container'))
+ReactDOM.render(<Image />, document.getElementById('react-container'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,55 @@
-const path = require("path")
+const path = require('path')
 
 module.exports = {
-    entry: "./src/index.js",
-    mode: "production",
+    context: path.resolve(__dirname, 'src'),
+    entry: './index.js',
+    mode: 'production',
     output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist")
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
             {
-                // look all the files that ends in 'js'
-                test: /.js$/,
+                // look all the files that ends in 'js' and 'jsx'
+                test: /.(js|jsx)$/,
+                // don't run node modules
                 exclude: /(node_modules)/,
                 use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["env", "react"]
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['es2015','react']
                     }
+                   
                 }
             },
             {
-                test:/\.scss$/,
-                use: [
-                    {loader: "style-loader"},
-                    {loader: "css-loader"},
-                    {loader: "sass-loader"}
-                ]
-            }
+                 // look all the files that ends in 'css' and 'scss'
+                test:/\.(css|scss)$/,
+                //using style/css/sass - loader 
+                loader: 'style-loader!css-loader!sass-loader?modules&localIdentName=[name]---[local]---[hash]' 
+                
+            },
+            {
+                test:/\.(jpeg|jpg|png|gif|svg)$/,
+                loader: 'url-loader',
+                options: {
+                    //a link referencing the file
+                    name:'[path][name].[ext]'
+                }
+                
+            },
+            {
+                test:/\.svg/,
+                use: {
+                    loader: 'url-loader',
+                    //If the file size is less then 1MB, file-loader will work as an alternative loader 
+                    options: {
+                        limit: 1000000,
+                        fallback:'file-loader?name=/[path][name].[ext]'
+                    }
+                }
+            } 
         ]
     }
 }
